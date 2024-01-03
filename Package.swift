976d56a61086ec8bc9577d6dac4f637607253b21
Samuel Défago <defagos@gmail.com>
2024-01-03 14:34:53 +0100

    Add dummy package with dummy products

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "Pillarbox",
+    platforms: [
+        .iOS(.v17),
+        .tvOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "Core",
+            targets: ["Core"]
+        ),
+        .library(
+            name: "Player",
+            targets: ["Player"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Core"
+        ),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "Player"
+        ),
+        .testTarget(
+            name: "PlayerTests",
+            dependencies: ["Player"]
+        )
+    ]
+)
---
+++ Tests/PlayerTests/PlayerTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Player
+
+final class PlayerTests: XCTestCase {
+    func testExample() throws {
+        
+    }
+}
